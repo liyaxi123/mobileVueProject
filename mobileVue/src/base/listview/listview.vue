7e464e5af02ba3c3c59c5604637c6b18ba17bebf
Louis <louis@sun-usb.com>
2019-07-17 19:17:55 +0800

    歌手页面完成2

--- mobileVue/src/base/listview/listview.vue
+++ mobileVue/src/base/listview/listview.vue
@@ -5,6 +5,20 @@
           :data="data"
           :class="listview"
           ref="listview">
+    <ul>
+      <li v-for="group in data" class="list-group" ref="listGroup">
+        <h2 class="list-group-title">{{group.title}}</h2>
+        <ul>
+          <li @click="selectItem(item)" v-for="item in group.items" class="list-group-item">
+            <img class="avatar" v-lazy="item.avatar">
+            <span class="name">{{item.name}}</span>
+          </li>
+        </ul>
+      </li>
+    </ul>
+    <div>
+
+    </div>
   </scroll>
 </template>
 
